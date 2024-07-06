5d7e175ff3a6016cdd1a79c16222c2a8e9432ccc
ced222 <79581884+ced222@users.noreply.github.com>
2024-07-06 21:38:44 +0700

    Co-authored-by: eroc123 <eroc123@users.noreply.github.com>

--- backend.py
+++ backend.py
@@ -1,5 +1,5 @@
 from cards import *
-
+import time
 #Basic game rules explanation
 '''
 Card types:
@@ -24,10 +24,10 @@ class Game():
     def __init__(self):
         '''Sets up a new game environment'''
         self.room = []
-        self.players = []
-        self.deck = Deck()
-        self.current_player_index = 0
-
+        self.players = {}
+        self.deck = Deck(self)
+        self.discardPile = DiscardedPile(self)
+        self.current_player_index = 1
     def add_player(self, player):
         '''
         When adding new players, first create a player using the player class
@@ -35,29 +35,51 @@ class Game():
         Then call this function, passing the new player object as a variable
         '''
         
-        self.players.append(player)
+        self.players[len(self.players)+1] = player
         self.room.append(player)
-        print(len(self.players))
+   
     
     def start_game(self):
         '''Call this function when the game is ready to start'''
-        for player in self.players:
+        for player in self.players.values():
             player.setup()
         while True:
             current_player = self.players[self.current_player_index]
-            current_player.on_turn()
-            self.current_player_index += 1
-            if self.current_player_index == len(self.players):
-                self.current_player_index = 0
+            if len(self.deck.getDeck()) == 0:
+                
+                self.deck.refillDeck(self.discardPile.getDeck())
+                self.deck.shuffleDeck()
+                
+
+            print("-----\nPlayer number {}'s turn\n-----".format(str(self.current_player_index)))
             
+            current_player.on_turn()
+            
+            
+            self.checkEliminations()
+            #win condition - all other players eliminated
+            if len(self.players) == 1:
+                print('Game over! Player {} has won the game!'.format(self.players.keys()[0]))
+            
+            self.current_player_index += 1
+            if self.current_player_index > len(self.players):
+                self.current_player_index = 1
+    def checkEliminations(self):
+        playersToEliminate = []
+        for playerIndex, player in self.players.items():
+                if player.health == 0:
+                    print("Player {} has been eliminated!".format(playerIndex)) #announce a players death
+                    playersToEliminate.append(playerIndex)
+        for playerIndex in playersToEliminate:
+            del self.players[playerIndex]
     def get_current_player(self):
         '''To get which player is currently performing actions'''
         return self.players[self.current_player_index]
 
     def get_next_player(self):
         next_index = self.current_player_index + 1
-        if next_index == len(self.players):
-            next_index = 0
+        if next_index > len(self.players):
+            next_index = 1
         return self.players[next_index]
 
 class Player():
@@ -66,15 +88,55 @@ class Player():
         self.hand = Hand(self)
         self.effect_zone = EffectZone(self)
         self.equipment_zone = EquipmentZone(self)
-
-    def on_turn(self):
-        prettyprint(self.hand)
-        choice = int(input(f'choose from deck above\nCard number : '))
-        self.hand[choice].on_play()
-
+        self.health = 0
 
     def setup(self):
-        self.hand.setup()
+        num_players = len(self.game.players)
+        for _ in range(init_cards[num_players]):
+            self.draw()
+        self.updateHealth() #update health stat
+    def updateHealth(self):
+        #give health to each player
+        self.health = 0
+        for card in self.hand:
+            
+            self.health += card.health
+    def on_turn(self):
+        self.updateHealth()
+        print("Current health: {}".format(self.health))
+        print("Current effects:")
+        if len(self.effect_zone) == 0:
+            print("None")
+        else:
+            prettyprint(self.effect_zone)
+        results = [Effects.on_effect() for Effects in self.effect_zone]
+        
+            
+        if 'skip' in results:
+            print('Player {}\'s turn skipped!'.format(self.game.current_player_index))
+            pass
+        else:
+            drawCard = input(f'Press any key to draw a card : ') #just as an input - later used for frontend card drawing
+            self.draw()
+            prettyprint(self.hand)
+            choice = int(input(f'choose from hand above\nCard number : '))
+            print("Card choice")
+            print(self.hand[choice-1].name)
+            successful_play = False
+            while not successful_play:
+                successful_play = self.hand[choice-1].on_play()
+            
+    
+        
+    #actions which a player can do 
+
+
+    def draw(self):
+        card = self.game.deck.draw() #self.deck is a class - draws card from deck
+        card.player = self #sets ownership of card to this player
+        self.hand.append(card) #initialize each card object (self is the hand of the player who drew it and self.next is the next player)
+   
+
 
 class Hand(list):
     def __init__(self, player):
@@ -82,44 +144,57 @@ class Hand(list):
         self.player = player
         self.name = 'hand'
 
-    def setup(self):
-        num_players = len(self.game.players)
-        for _ in range(init_cards[num_players]):
-            self.draw()
-
-    def draw(self,):
-        card = self.game.deck.draw() #self.deck is a class - draws card from deck
-        self.append(card(self)) #initialize each card object (self is the hand of the player who drew it and self.next is the next player)
 
 class EffectZone(list):
     def __init__(self, deck):
         self.deck = deck
-        self.name = 'effect zone'
+        self.name = 'effect_zone'
     
 class EquipmentZone(list):
     def __init__(self, deck):
         self.deck = deck
-        self.name = 'equipment zone'
+        self.name = 'equipment_zone'
         
 class Deck(list):
-    def __init__(self,):
-        # fill up deck, this is just random numbers not the final
-        [self.append(seed_of_life) for _ in range(4)]
-        [self.append(arrow) for _ in range(13)]
-        # for health varients, e.g.
-        card = seed_of_life
+    def __init__(self, game):
+        super().__init__()
+        self.game = game
+        
+        # Populate deck with instances of cards
+        for _ in range(6):
+            self.append(seed_of_life(None))  # Assuming seed_of_life is a class and requires no arguments
+        
+        for _ in range(13):
+            self.append(arrow_ice(None))  # Assuming arrow is a class and requires no arguments
+        
+        # Example of adding a variant
+        card = seed_of_life(None)
         card.health += 1
-        card.image += '_health' # etc 
+        card.image += '_health'
         self.append(card)
         
-        
-    def draw(self,):
-        card_index = random.randint(0,len(self)-1)
-        card = self[card_index]
-        self.pop(card_index)
-        return card
-    
+        self.shuffleDeck()  # Optionally shuffle the deck after initialization
 
+    def draw(self):
+        if not self:
+            raise IndexError("Deck is empty, cannot draw.")
+        
+        card_index = random.randint(0, len(self) - 1)
+        card = self.pop(card_index)
+        return card
+    def refillDeck(self, cards):
+        self += cards
+    def shuffleDeck(self):
+        random.shuffle(self)
+    def getDeck(self):
+        return self
+class DiscardedPile(list):
+    def __init__(self, game):
+        self.game = game
+        self.discardPile = []
+    def getDeck(self):
+        return self.discardPile
+    
 
 
 # testing code below
--- player.py
+++ player.py
@@ -1,2 +1,19 @@
 ## PLAYER ##
 
+
+import socket
+
+
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('100.88.125.61', 10000))
+    data = input()
+    if data == '':
+        s.close()
+        continue
+    s.send(data.encode())
+    data = s.recv(64).decode()
+    print(data)
+    s.close()
+    if data == 'exit ack':
+        break
