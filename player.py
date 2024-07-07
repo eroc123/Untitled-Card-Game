a00e27571aa5032be6aa89e46f733659963e631c
eroc123 <79434990+eroc123@users.noreply.github.com>
2024-07-07 21:09:03 +0700

    Attempted networking

--- player.py
+++ player.py
@@ -1,2 +1,18 @@
 ## PLAYER ##
 
+import socket
+
+
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('thinkpad-t14s.tailc3214.ts.net', 10000))
+    data = input()
+    if data == '':
+        s.close()
+        continue
+    s.send(data.encode())
+    data = s.recv(64).decode()
+    print(data)
+    s.close()
+    if data == 'exit ack':
+        break
--- server.py
+++ server.py
@@ -1,5 +1,15 @@
-## SERVER FILE ## 
-
-# networking stuff here
-
 import socket
+import backend
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('127.0.0.1', 10000))
+serversocket.listen(5) 
+
+while True:
+    connection, address = serversocket.accept()
+    buf = connection.recv(64)
+    if len(buf) > 0:
+        print(buf, address)
+        connection.sendall(buf + b' ack')
+        if buf == b'exit':
+            break
